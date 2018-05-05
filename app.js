cb8acbb7d7129ec37901afbccfbcb0c1ec2ccc5b
Wing Chau <wing@devtography.com>
2018-05-05 17:16:46 +0100

    Basic routing

--- app.js
+++ app.js
@@ -1,7 +1,33 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
+const index = require('./routes/index');
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(express.static('public'));
+app.use('/', index);
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use((err, req, res) => {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
 
 app.listen(3000);
+
+module.exports = app;
