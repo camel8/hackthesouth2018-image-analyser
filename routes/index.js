131ad309160d565b9d639f232270d5d51b1f23db
Wing Chau <wing@devtography.com>
2018-05-06 04:32:38 +0100

    Implemented files upload

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,9 @@
 const express = require('express');
+const multer = require('multer');
+
+const upload = multer({
+  storage: multer.memoryStorage(),
+});
 
 const router = express.Router();
 
@@ -7,6 +12,14 @@ router.get('/', (req, res) => {
   res.render('index', {
     title: 'The n00bs',
   });
+});
+
+router.post('/extractText', upload.single('datafile'), (req, res) => {
+  if (!req.file) {
+    return res.send('failed');
+  }
+
+  return res.send(req.file);
 });
 
 module.exports = router;
